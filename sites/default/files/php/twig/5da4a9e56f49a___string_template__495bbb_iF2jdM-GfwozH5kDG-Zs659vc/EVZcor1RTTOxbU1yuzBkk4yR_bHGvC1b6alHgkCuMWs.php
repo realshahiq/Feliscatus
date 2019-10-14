c2e955dba3ca4acbcca14ca08d543538f730e14f
SyedShahiq <shahiqurrehman@gmail.com>
2019-10-14 22:03:18 +0500

    Files updated

---
+++ sites/default/files/php/twig/5da4a9e56f49a___string_template__495bbb_iF2jdM-GfwozH5kDG-Zs659vc/EVZcor1RTTOxbU1yuzBkk4yR_bHGvC1b6alHgkCuMWs.php
@@ -0,0 +1,106 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__495bbbe854af5418f4c6e42ffe45248abd8185ca8b186f1320857dada1a9c7a6 */
+class __TwigTemplate_df4566f586c8d792804d938fe2ee09a7c96e2b6c494829926c41edb84a0ab77d extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = ["escape" => 3];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<div class='row'>
+<div class='col-lg-4 col-md-4 col-sm-12 col-xs-12'>
+<div>";
+        // line 3
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["field_image"] ?? null)), "html", null, true);
+        echo "</div>
+</div>
+<div class='col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+<h4>";
+        // line 6
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null)), "html", null, true);
+        echo "</h4>
+<div class='blog-body'>";
+        // line 7
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["body"] ?? null)), "html", null, true);
+        echo "</div>
+<div>";
+        // line 8
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["view_node"] ?? null)), "html", null, true);
+        echo "</div>
+</div>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__495bbbe854af5418f4c6e42ffe45248abd8185ca8b186f1320857dada1a9c7a6";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  73 => 8,  69 => 7,  65 => 6,  59 => 3,  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__495bbbe854af5418f4c6e42ffe45248abd8185ca8b186f1320857dada1a9c7a6", "");
+    }
+}
